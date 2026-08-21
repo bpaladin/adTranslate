4249ccfe394a8abc389a05ee68a0a641b8acacb6
bpaladin <black_paladin@mail.ru>
2026-08-21 16:47:52 +0300

    10

--- batch_translator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for pdf_file in ./pdfs/*.pdf; do
-    if [ -f "$pdf_file" ]; then
-        filename=$(basename "$pdf_file" .pdf)
-        python main.py "$pdf_file" "./pdfs/${filename}.html"
-    fi
-done
